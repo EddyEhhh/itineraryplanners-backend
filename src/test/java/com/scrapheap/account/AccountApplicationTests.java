d3eb6253de6ea39d91138101a8b3cbde16b19bf5
EddyEhhh <ehhitseddy@gmail.com>
2023-05-25 16:17:51 +0800

    Initial commit

---
+++ src/test/java/com/scrapheap/account/AccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scrapheap.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
