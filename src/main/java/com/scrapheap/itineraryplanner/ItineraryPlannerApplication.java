8076b3ea93567e43e19ca99c6fdc12c8836bedfa
wfanggg <120258509+wfanggg@users.noreply.github.com>
2023-07-24 11:17:40 +0800

    Test build.yml

--- src/main/java/com/scrapheap/itineraryplanner/ItineraryPlannerApplication.java
+++ src/main/java/com/scrapheap/itineraryplanner/ItineraryPlannerApplication.java
@@ -10,7 +10,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableAsync
 public class ItineraryPlannerApplication {
 
-
+// Testing123
     public static void main(String[] args) {
         SpringApplication.run(ItineraryPlannerApplication.class, args);
     }
